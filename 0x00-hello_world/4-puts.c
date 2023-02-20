16525dacacc84f8013ddfa3a3f77bfe4e13d9d89
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-02-20 00:38:11 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Printing  Programming is like building a multilingual puzzle using puts function
+ * main - programming is like building a multilingual puzzle wit puts
  * Return: 0
  */
 int main(void)
