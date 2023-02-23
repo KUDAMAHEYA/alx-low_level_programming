72c7b3866c7f760b0bf4640c725f80fcf6a6e14b
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-02-22 16:37:39 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char ab = 'a';
+
+	while (ab <= 'z')
+	{
+		_putchar(c);
+		ab++;
+
+	}
+	_putchar('\n');
+
+}
