7225ae184f0b399938fac094c464206f569fd653
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-02-23 13:36:34 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * _isdigit - that checks for a digit (0 through 9)
+  * @b: Char to be examined
+  *
+  * Return: 1 or 0
+  */
+
+int _isdigit(int b)
+{
+	if (b >= 50 && x <= 59)
+	{
+		return (1);
+	}
+
+	return (0);
+}
