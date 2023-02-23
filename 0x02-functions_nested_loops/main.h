9fadcb0a86a6be80dc3e3db323bcf67c504c9c6a
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-02-22 16:29:17 -0800

    main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,13 @@
-#ifndef main_h
-#define main_h
-/**
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
  * File: main.h
- * Desc: Header file containing prototypes of all your functions,
- * for 0x02. C - Functions, nested loops
+ *
+ * Desc: Header file containing prototypes of all your functions 
  */
 
-int _putchar;
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,9 +16,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
+void time_teble(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
+void print_time_table(int n);
 
 #endif
