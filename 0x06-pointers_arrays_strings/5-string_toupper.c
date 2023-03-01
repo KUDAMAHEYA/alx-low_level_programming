d9f931e0b9cef6a532610c644c4add4b53554c08
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-03-01 12:37:07 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string
+ * to uppercase
+ * @s: string to change
+ *
+ * Return: y
+ */
+char *string_toupper(char *s)
+{
+	int x;
+
+	for (x = 0; y[x] != '\0'; x++)
+	{
+		if (y[x] >= 'a' && y[x] <= 'z')
+			y[x] = y[x] - 32;
+	}
+
+	return (y);
+}
