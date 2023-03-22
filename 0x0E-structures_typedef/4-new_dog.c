6b216f8c1c273c0599c142a0fb173945683538a9
KUDAMAHEYA <Milaceymilacey@gmail.com>
2023-03-22 11:44:00 -0700

    0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to examine
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int leng;
+
+	leng = 0;
+
+	while (s[leng] != '\0')
+	{
+		leng++;
+	}
+
+	return (leng);
+}
+
+/**
+*_strcopy - copy string pointed by src
+*into dest variable
+*@dest:buffer storing string copy
+*@src: buffer storing string to copy
+*Return:returns copied string
+*/
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+
+for (; src[index] ; index++)
+dest[index] = src[index];
+
+dest[index] = '\0';
+return (dest);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog (Success), NULL otherwise
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,21 +9,11 @@
  *
  * Description: The characteristic of a dog.
  */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-/**
- * dog_t - Typedef for dog structure
- */
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-
-#endif
+	if (dog == NULL)
+		dog = malloc(sizeof(struct dog));
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+}
